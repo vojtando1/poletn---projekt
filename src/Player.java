2354aa5d559b4564681703129858cd6a4a2cb31f
vojta <vojtauhlik08@gmail.com>
2026-01-21 16:19:48 +0100

    created player class without javaDocs for now

---
+++ src/Player.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class Player {
+    private  String name;
+    private Location location;
+    boolean knowsWeakness = false;
+    private  ArrayList<Item> inventory = new ArrayList<>();
+
+
+    public Player(String name, Location start) {
+        this.name = name;
+        this.location = start;
+    }
+
+
+    public Location getLocation() { return location; }
+    public void setLocation(Location l) { location = l; }
+
+
+    public void addItem(Item i) { inventory.add(i); }
+
+
+    public Item getItem(String id) {
+        return inventory.stream().filter(i -> i.id.equals(id)).findFirst().orElse(null);
+    }
+
+
+    public boolean hasItem(String id) {
+        return getItem(id) != null;
+    }
+
+
+    public String inventoryToString() {
+        if (inventory.isEmpty()) return "Inventář je prázdný.";
+        StringBuilder sb = new StringBuilder("Inventář: ");
+        for (Item i : inventory) sb.append(i.name).append(", ");
+        return sb.toString();
+    }
+}

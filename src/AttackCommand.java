bcf54f1503083bc71948bb06c220423ed7912822
vojta <vojtauhlik08@gmail.com>
2026-01-28 16:51:23 +0100

    attack command

---
+++ src/AttackCommand.java
@@ -0,0 +1,14 @@
+public class AttackCommand implements Command {public void execute(Game game, String argument) {
+    GameCharacter g = game.getPlayer().getLocation().getCharacter("golem");
+    if (g == null) {
+        System.out.println("Není na koho útočit.");
+        return;
+    }
+    if (!game.getPlayer().hasItem("stary_mec") || !game.getPlayer().knowsWeakness) {
+        System.out.println("Nemáš vybavení nebo znalosti k poražení Golema.");
+        return;
+    }
+    System.out.println("Porazil jsi Golema! Objevuje se portál. Vyhrál jsi!");
+    game.stop();
+}
+}

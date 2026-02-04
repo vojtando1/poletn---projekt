6d3f8c47c176d4c59e60e254e295f13f78437e2b
vojta <vojtauhlik08@gmail.com>
2026-02-04 16:49:39 +0100

    added return

--- src/TakeCommand.java
+++ src/TakeCommand.java
@@ -1,14 +1,19 @@
 public class TakeCommand implements Command {
+
+    @Override
     public String execute(Game game, String argument) {
-    Item i = game.getPlayer().getLocation().takeItem(argument);
-    if (i == null) System.out.println("Takový předmět tu není.");
-    else { game.getPlayer().addItem(i); }
-    return "Sebral jsi " + i.getName();
-}
+        Item i = game.getPlayer().getLocation().takeItem(argument);
+
+        if (i == null) {
+            return "Takový předmět tu není nebo jej nelze sebrat.";
+        }
+        else
+          game.getPlayer().addItem(i);
+        return "Sebral jsi " + i.getName();
+    }
 
     @Override
     public boolean exit() {
         return false;
     }
 }
-

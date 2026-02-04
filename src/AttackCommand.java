06a11adc33f3face3e2b68b2a95c80cc6222c0e5
vojta <vojtauhlik08@gmail.com>
2026-02-04 16:30:43 +0100

    player can now attack golem, also he can die

--- src/AttackCommand.java
+++ src/AttackCommand.java
@@ -1,20 +1,33 @@
 public class AttackCommand implements Command {
+    @Override
     public String execute(Game game, String argument) {
-        GameCharacter g = game.getPlayer().getLocation().getCharacter("golem");
+        Location loc = game.getPlayer().getLocation();
+        GameCharacter g = loc.getCharacter("golem");
+
         if (g == null) {
             return "Není na koho útočit.";
         }
-        if (!game.getPlayer().hasItem("stary_mec") || !game.getPlayer().knowsWeakness) {
-            return "Nemáš vybavení nebo znalosti k poražení Golema.";
-        }
-        game.stop();
-        return "Porazil jsi Golema! Objevuje se portál. Vyhrál jsi!";
 
+        Player p = game.getPlayer();
+
+        if (!p.hasItem("stary_mec") || !p.knowsWeakness) {
+            p.damage(5);
+
+            if (!p.isAlive()) {
+                game.stop();
+                return "Golem tě zabil. Prohrál jsi.";
+            }
+
+            return "Útočíš, ale Golem je příliš silný! Ztrácíš 5 HP (HP: "
+                    + p.getHealth() + ")";
+        }
+
+        loc.getCharacters().remove(g);
+        return "Porazil jsi Golema! Objevuje se portál.";
     }
 
     @Override
     public boolean exit() {
         return false;
     }
-
 }

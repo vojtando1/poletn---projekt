88d40284632f0bb906fcb44986af15fd3fbf1d17
vojta <vojtauhlik08@gmail.com>
2026-02-11 16:37:19 +0100

    javaDocs 👌

--- src/Commands/Command.java
+++ src/Commands/Command.java
@@ -1,6 +1,37 @@
 package Commands;
-import Game.*;
+import Game.Game;
+
+/**
+ * Rozhraní představující obecný příkaz ve hře.
+ * <p>
+ * Každý příkaz ve hře (např. jdi, vezmi, použij, útoč, konec)
+ * implementuje toto rozhraní. Díky tomu může hra pracovat
+ * s příkazy jednotným způsobem.
+ * </p>
+ *
+ * Implementující třídy musí definovat:
+ * <ul>
+ *     <li>logiku provedení příkazu</li>
+ *     <li>informaci, zda příkaz ukončuje hru</li>
+ * </ul>
+ *
+ * @author Vojta
+ */
 public interface Command {
+
+    /**
+     * Provede konkrétní herní příkaz.
+     *
+     * @param game aktuální instance hry obsahující stav hráče a světa
+     * @param argument textový argument zadaný uživatelem (např. cíl pohybu)
+     * @return textová odpověď hry, která se zobrazí hráči
+     */
     String execute(Game game, String argument);
+
+    /**
+     * Určuje, zda příkaz ukončuje hru.
+     *
+     * @return true pokud příkaz ukončuje hru, jinak false
+     */
     boolean exit();
 }

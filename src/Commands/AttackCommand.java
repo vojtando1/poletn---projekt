9abacfbbd8f95851e98c9bd1df3d07ba08d1773c
vojta <vojtauhlik08@gmail.com>
2026-02-11 16:29:13 +0100

    added javaDocs, there are HTML syntaxes for formating lists :)

--- src/Commands/AttackCommand.java
+++ src/Commands/AttackCommand.java
@@ -1,10 +1,37 @@
 package Commands;
 import Game.*;
 
+/**
+ * Příkaz umožňující hráči zaútočit na nepřítele v aktuální lokaci.
+ * <p>
+ * Tento příkaz je primárně určen pro boj s bossem hry – Golemem.
+ * Hráč může Golema porazit pouze v případě, že:
+ * <ul>
+ *     <li>vlastní předmět "stary_mec"</li>
+ *     <li>zná slabinu Golema (proměnná knowsWeakness)</li>
+ * </ul>
+ * Pokud hráč nesplňuje podmínky, Golem mu způsobí zranění.
+ * Při ztrátě všech životů hra končí prohrou.
+ * </p>
+ *
+ * @author Vojta
+ */
 public class AttackCommand implements Command {
+
+    /**
+     * Provede útok hráče na postavu v aktuální lokaci.
+     *
+     * @param game     aktuální instance hry obsahující herní data a stav hráče
+     * @param argument argument příkazu (aktuálně není využíván)
+     * @return textová zpráva popisující výsledek útoku
+     */
     @Override
     public String execute(Game game, String argument) {
+
+        // Získání aktuální lokace hráče
         Location loc = game.getPlayer().getLocation();
+
+        // Pokus o nalezení Golema v lokaci
         GameCharacter g = loc.getCharacter("golem");
 
         if (g == null) {
@@ -13,9 +40,13 @@ public class AttackCommand implements Command {
 
         Player p = game.getPlayer();
 
+        // Kontrola, zda má hráč potřebné vybavení a znalosti
         if (!p.hasItem("stary_mec") || !p.knowsWeakness) {
+
+            // Hráč utrpí zranění
             p.damage(5);
 
+            // Pokud hráč zemře, hra končí
             if (!p.isAlive()) {
                 game.stop();
                 return "Golem tě zabil. Prohrál jsi.";
@@ -25,10 +56,17 @@ public class AttackCommand implements Command {
                     + p.getHealth() + ")";
         }
 
+        // Golem je poražen
         loc.getCharacters().remove(g);
+
         return "Porazil jsi Golema! Objevuje se portál.";
     }
 
+    /**
+     * Určuje, zda příkaz ukončuje hru.
+     *
+     * @return false – tento příkaz hru přímo neukončuje
+     */
     @Override
     public boolean exit() {
         return false;
